eb84c86b4013f479083c78809b760e03647edc8d
bankolejohn <bankolejohn@gmail.com>
2024-04-29 07:34:03 +0100

    initial commit

---
+++ com.epistimis.uddl/script.js
@@ -0,0 +1,15 @@
+// Smooth scrolling for table of contents links
+document.querySelectorAll('.toc a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
